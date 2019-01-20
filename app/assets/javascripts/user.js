cdeeea4c20b8c20e21144aa36fdd82ce56e6d616
ainsoph567 <ainsoph567@gmail.com>
2019-01-20 20:22:26 +0900

    delete unnecessary cord

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -20,7 +20,6 @@ $(function() {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    // var groupId = $('.chat__group_id').val();
     $.ajax({
       type: 'GET',
       url: '/users',
